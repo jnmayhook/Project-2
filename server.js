231052ab429eb6203dd6988d1e2ab4644e2db3d6
Drew Payton <drewpayton18@gmail.com>
2022-07-10 09:17:25 -0500

    updating the server.js so the server can now boot up and added a public folder with style.css file in it

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const session = require('express-session');
+const exhbs = require('express-handlebars');
+const path = require('path');
+const sequelize = require('sequelize');
+const hbs = exhbs.create({})
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const sess = {
+    secret: 'Super Secret Secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize,
+      }),
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => console.log(`The server has started on http://localhost:${PORT}`))
+})
+
