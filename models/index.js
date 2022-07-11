efbec43869b2994716ca8c22476eee19d6561a5c
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-10 19:49:29 -0700

    Created model relationships

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const User = require("./User"); 
+const Post = require("./Post"); 
+const Comment = require("./Comment"); 
+// const Topic = require("./Topic");
+
+User.hasMany(Post, {
+    foreignKey: "user_id", 
+    onDelete: "CASCADE"
+}); 
+
+User.hasMany(Comment, {
+    foreignKey: "user_id", 
+    onDelete: "CASCADE"
+}); 
+
+Post.hasMany(Comment, {
+    foreignKey: "project_id",
+    onDelete: "CASCADE"
+}); 
+
+Post.belongsTo(User, {
+    foreignKey: "user_id"
+}); 
+
+Comment.belongsTo(Post, {
+    foreignKey: "project_id"
+}); 
+
+Comment.belongsTo(User, {
+    foreignKey: "user_id"
+}); 
