05ab0b6310bf092a87ee67abfee4c93003c6c1d5
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-12 20:34:25 -0700

    Modified code to be fit for testing

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,12 +1,17 @@
 const router = require("express").Router(); 
 const { User } = require("../../models");
 
+// create a new user
+// TEST VERSION FOR INSOMNIA TESTING
 router.post("/", async (req, res) => {
     try {
-        const userData = await User.create(req.body); 
+        const userData = await User.create({
+            username: req.body.username,
+            password: req.body.password
+        }); 
 
         req.session.save(() => {
-            req.session.user_id = userData.id; 
+            // req.session.user_id = userData.id; 
             req.session.logged_in = true; 
 
             res.status(200).json(userData); 
