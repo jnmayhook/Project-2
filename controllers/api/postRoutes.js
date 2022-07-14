f6f6be1974c49e48d27a5fca21ecef752dd816b1
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-14 14:02:40 -0700

    Created DELETE posts route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router(); 
 const { Post } = require("../../models");
 
-router.post("/", async (req, res) => {
+router.post("/", async (req, res) => { //add authorization helper later
     try {
         const newPost = await Post.create({
-            // Placeholder until frontend FETCH requests are made - to be replaced with [...req.body} later
+            // Placeholder until frontend FETCH requests are made - to be replaced with [...req.body] later
             title: req.body.title, 
             post_text: req.body.post_text,
             user_id: req.session.user_id,
@@ -15,5 +15,23 @@ router.post("/", async (req, res) => {
     }
 }); 
 
+router.delete("/:id", async (req, res) => {
+    try {
+        const deletedPost = await Post.destroy({
+            where: {
+                id: req.params.id, 
+                user_id: req.session.user_id
+            }, 
+        }); 
+
+        if (!deletedPost) {
+            res.status(404).json({ message: "No post found with this ID."}); 
+            return; 
+        }
+        res.status(200).json(deletedPost);
+    } catch (err) {
+        res.status(500).json(err); 
+    }
+})
 
 module.exports = router;
