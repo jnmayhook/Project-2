a1306bc7587c955db685ddb3d7d1bc5b6a11d89d
Adeline <adelineaguspranoto@gmail.com>
2022-07-14 17:59:46 -0700

    adding checkPassword

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -69,5 +69,4 @@ router.post('/login', async (req, res) => {
   }
 });
 
-
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -3,7 +3,7 @@ const homeRoutes = require('./home-routes');
 const apiRoutes = require("./api"); 
 
 router.use('/', homeRoutes); 
-router.use("/api", apiRoutes); 
+router.use('/api', apiRoutes); 
 
 
 module.exports = router
