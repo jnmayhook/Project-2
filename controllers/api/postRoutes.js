b7054f2bbd8deee0c3e004fdfb155cce60247e8f
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-15 14:39:06 -0700

    Created PUT route

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,37 +1,60 @@
-const router = require("express").Router(); 
+const router = require("express").Router();
 const { Post } = require("../../models");
-const withAuth = require("../../utils/auth"); 
+const withAuth = require("../../utils/auth");
 
-router.post("/", withAuth, async (req, res) => { 
+router.post("/", withAuth, async (req, res) => {
     try {
         const newPost = await Post.create({
             // Placeholder until frontend FETCH requests are made - to be replaced with [...req.body] later
-            title: req.body.title, 
+            title: req.body.title,
             post_text: req.body.post_text,
             user_id: req.session.user_id,
-        }); 
-        res.status(200).json(newPost); 
+        });
+        res.status(200).json(newPost);
     } catch (err) {
-        res.status(400).json(err);
+        res.status(500).json(err);
     }
-}); 
+});
+
+router.put("/:id", withAuth, async (req, res) => {
+    try {
+        const updatedPost = await Post.update({
+            //Placeholder
+            title: req.body.title,
+            post_text: req.body.post_text
+        },
+            {
+                where: {
+                    id: req.params.id,
+                }
+            });
+
+        if (!updatedPost) {
+            res.status(404).json({ message: "No post found with this ID." });
+            return;
+        };
+        res.status(200).json(updatedPost);
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 router.delete("/:id", withAuth, async (req, res) => {
     try {
         const deletedPost = await Post.destroy({
             where: {
-                id: req.params.id, 
-                user_id: req.session.user_id
-            }, 
-        }); 
+                id: req.params.id,
+            },
+        });
 
         if (!deletedPost) {
-            res.status(404).json({ message: "No post found with this ID."}); 
-            return; 
+            res.status(404).json({ message: "No post found with this ID." });
+            return;
         }
         res.status(200).json(deletedPost);
     } catch (err) {
-        res.status(500).json(err); 
+        res.status(500).json(err);
     }
 })
 
