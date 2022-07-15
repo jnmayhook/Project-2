6efe2524a403a65a14e8e399625c7488c8fa42d6
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-14 19:39:29 -0700

    Created GET route to view all comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,24 +1,35 @@
-const router = require("express").Router(); 
-const { Comment } = require("../../models");
+const router = require("express").Router();
+const { json } = require("sequelize/types");
+const { User, Post, Comment } = require("../../models");
 
 router.get("/", async (req, res) => {
     try {
         const allComments = await Comment.findAll({
-            where: 
+            include: [
+                {
+                    model: Post,
+                    include: {
+                        model: User,
+                        attributes: { exclude: ["password"] },
+                    }
+                },
+
+            ]
         })
-    } catch (err) {}
+    } catch (err) { }
 })
 
 router.post("/", async (req, res) => {
     try {
         const newComment = await Comment.create({
             // Placeholder until frontend made
-            comment_text: req.body.comment_text, 
-            post_id: req.body.post_id, 
+            comment_text: req.body.comment_text,
+            post_id: req.body.post_id,
             user_id: req.session.user_id
-        }); 
-        res.status(200).json(newPost); 
+        });
+        res.status(200).json(newPost);
     } catch (err) {
-        res.status(400).json(err); 
+        res.status(400).json(err);
     }
 }); 
+
