bc27f5eb62158d5c7752cfb65579a433f28539ed
reversedentistry <99564415+reversedentistry@users.noreply.github.com>
2022-07-28 19:38:12 -0700

    Removed import

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,6 @@
 const router = require('express').Router();
 const homeRoutes = require('./home-routes');
 const apiRoutes = require("./api"); 
-const userPosts = require("./userPosts"); 
 
 router.use('/', homeRoutes); 
 router.use('/api', apiRoutes); 
